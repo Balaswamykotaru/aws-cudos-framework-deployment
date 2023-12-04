430219a5dc8596e569b4c2c3f669d1b827359a66
Balaswamykotaru <132573107+Balaswamykotaru@users.noreply.github.com>
2023-12-04 17:02:11 +0530

    Update main.tf

--- terraform-modules/cur-setup-destination/main.tf
+++ terraform-modules/cur-setup-destination/main.tf
@@ -40,7 +40,7 @@ resource "aws_s3_bucket_public_access_block" "this" {
 resource "aws_s3_bucket_versioning" "this" {
   bucket = aws_s3_bucket.this.bucket
   versioning_configuration {
-    status = "Disabled"
+    status = "Enabled"
   }
 }
 
