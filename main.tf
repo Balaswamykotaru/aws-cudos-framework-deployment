aff1498c2bf0a105fef3debafe29731ebb9730a5
Balaswamykotaru <132573107+Balaswamykotaru@users.noreply.github.com>
2023-12-06 10:33:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ provider "aws" {
 module "cur_destination" {
   source = "github.com/aws-samples/aws-cudos-framework-deployment//terraform-modules/cur-setup-destination"
   source_account_ids = ["797078318809","611960772844","450683952831"]
-  create_cur         = flase # Set to true to create an additional CUR in the aggregation account
+  create_cur         = true # Set to true to create an additional CUR in the aggregation account
 
   # Provider alias for us-east-1 must be passed explicitly (required for CUR setup)
   providers = {
