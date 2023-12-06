8dbeeccf3fae9d426a4efb1673d86c6d6638f013
Balaswamykotaru <132573107+Balaswamykotaru@users.noreply.github.com>
2023-12-06 11:28:54 +0530

    Update varibles.tf

--- varibles.tf
+++ varibles.tf
@@ -4,11 +4,11 @@
 
 variable "src1_role_arn" {
   type        = string
-  description = "The ARN of the role you assume for deploying the component "
+  
 }
 
 variable "src2_role_arn" {
   type        = string
-  description = "Source account id to store Cost and Usage reports"
+  
 }
 
