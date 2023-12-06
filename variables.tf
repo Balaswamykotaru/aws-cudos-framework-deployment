ebf2d45c55731ff80e09a6f24e75979f43ad6aca
Balaswamykotaru <132573107+Balaswamykotaru@users.noreply.github.com>
2023-12-06 14:30:11 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,11 +4,18 @@
 
 variable "src1_role_arn" {
   type        = string
-  
+  }
+variable "src1_account_id" {
+  type        = string
+  description = "Source account id to store Cost and Usage reports"
 }
 
 variable "src2_role_arn" {
   type        = string
-  
 }
+variable "src2_account_id" {
+  type        = string
+  description = "Source account id to store Cost and Usage reports"
+}
+
 
