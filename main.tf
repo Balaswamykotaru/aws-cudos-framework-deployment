a31c80551c254e9859508d69abd08a8d8be30456
Balaswamykotaru <132573107+Balaswamykotaru@users.noreply.github.com>
2023-12-06 16:02:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -67,3 +67,18 @@ provider "aws" {
   }
 }
 
+# cid_dashboards 
+module "cid_dashboards" {
+    source = "github.com/aws-samples/aws-cudos-framework-deployment//terraform-modules/cid-dashboards"
+    stack_name      = "Cloud-Intelligence-Dashboards"
+    template_bucket = "mytestbucket123332"
+  
+    stack_parameters = {
+      "PrerequisitesQuickSight"            = "yes"
+      "PrerequisitesQuickSightPermissions" = "yes"
+      "QuickSightUser"                     = "QS-Admin-1/QS-Admin"
+      "DeployCUDOSDashboard"               = "yes"
+      "DeployCostIntelligenceDashboard"    = "yes"
+      "DeployKPIDashboard"                 = "yes"
+    }
+}
