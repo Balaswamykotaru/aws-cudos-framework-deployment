cdf5a9da427faf532307be93eb18ca94e900e26e
Balaswamykotaru <132573107+Balaswamykotaru@users.noreply.github.com>
2023-12-06 15:38:32 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,20 +2,22 @@
 # CUDOS  variable definition
 #------------------------------------------------------------------------------#
 
-#variable "src1_role_arn" {
- # type        = string
-  #}
-#variable "src1_account_id" {
- # type        = string
-  #description = "Source account id to store Cost and Usage reports"
-#}
+variable "src1_role_arn" {
+  type        = string
+  description = "Source account id for CUDOS"
+  }
+variable "src1_account_id" {
+  type        = string
+  description = "Source account id to store CUDOS"
+}
 
 variable "src2_role_arn" {
   type        = string
+  description = "Source account id for CUDOS"
 }
 variable "src2_account_id" {
   type        = string
-  description = "Source account id to store Cost and Usage reports"
+  description = "Source account id to store CUDOS"
 }
 
 
