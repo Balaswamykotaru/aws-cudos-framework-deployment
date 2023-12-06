d81a320a4a2467953a41f1edf9e8a0f1ecba974f
Balaswamykotaru <132573107+Balaswamykotaru@users.noreply.github.com>
2023-12-06 15:01:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,28 +17,6 @@ module "cur_destination" {
     aws.useast1 = aws.useast1
   }
 }
-data "aws_caller_identity" "scr1" {
-  provider = aws.src1
-}
-provider "aws" {
-  #profile = "src1"
-  region  = "eu-central-1"
-  #shared_credentials_files  = ["C:/Users/balaswamy.kotaru/.aws/credentials"]
-  alias   = "src1"
-  assume_role {
-    role_arn = var.src1_role_arn
-  }
-}
-
-provider "aws" {
-  #profile = "src1"
-  region  = "us-east-1"
-  #shared_credentials_files  = ["C:/Users/balaswamy.kotaru/.aws/credentials"]
-  alias   = "src1_useast1"
-  assume_role {
-    role_arn = var.src1_role_arn
-  }
-}
 
 provider "aws" {
   #profile = "src2"
@@ -59,19 +37,6 @@ provider "aws" {
     role_arn = var.src2_role_arn
   }
  }
-# Configure one or more source (payer) accounts
- module "cur_source1" {
-  #source = "github.com/aws-samples/aws-cudos-framework-deployment//terraform-modules/cur-setup-source"
-  source = "./terraform-modules/cur-setup-source"
-  destination_bucket_arn = module.cur_destination.cur_bucket_arn
-
-  # Provider alias for us-east-1 must be passed explicitly (required for CUR setup)
-  # Optionally, you may pass the default aws provider explicitly as well
-  providers = {
-    aws         = aws.src1
-    aws.useast1 = aws.src1_useast1
-  }
-}
 
  module "cur_source2" {
   source = "github.com/aws-samples/aws-cudos-framework-deployment//terraform-modules/cur-setup-source"
