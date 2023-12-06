9dff9b334605ed573ca745c9225aa7663f68b96f
Balaswamykotaru <132573107+Balaswamykotaru@users.noreply.github.com>
2023-12-06 11:21:39 +0530

    Update and rename vars.tf to varibles.tf

--- varibles.tf
+++ varibles.tf
@@ -4,7 +4,7 @@
 
 variable "src1_role_arn" {
   type        = string
-  description = "The ARN of the role you assume for deploying the component -
+  description = "The ARN of the role you assume for deploying the component "
 }
 
 variable "src2_role_arn" {
@@ -12,7 +12,3 @@ variable "src2_role_arn" {
   description = "Source account id to store Cost and Usage reports"
 }
 
-#variable "src1_role_arn" {
-#  type        = string
-#  description = "The ARN of the source account id to store Cost and Usage reports "
-#}
