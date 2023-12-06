8a48a7612ababc37f55dbecd29f64c7dc473b64e
Balaswamykotaru <132573107+Balaswamykotaru@users.noreply.github.com>
2023-12-06 17:03:20 +0530

    Update and rename modules.txt to dst.tf

--- terraform-modules/cur-setup-destination/dst.tf
+++ terraform-modules/cur-setup-destination/dst.tf
@@ -1,19 +1,18 @@
-/provider "aws" {
+provider "aws" {
+  alias   = "dst"
   region = "eu-central-1"
 }
-
 provider "aws" {
   region = "us-east-1"
-  alias  = "useast1"
+  alias  = "dst_useast1"
 }
-
+# Destination account setup
 module "cur_destination" {
   source = "github.com/aws-samples/aws-cudos-framework-deployment//terraform-modules/cur-setup-destination"
   source_account_ids = ["797078318809","611960772844","450683952831"]
-  create_cur         = flase # Set to true to create an additional CUR in the aggregation account
-
+  create_cur         = false # Set to true to create an additional CUR in the aggregation account
   # Provider alias for us-east-1 must be passed explicitly (required for CUR setup)
   providers = {
-    aws.useast1 = aws.useast1
+    aws.useast1 = aws.dst_useast1
   }
 }
